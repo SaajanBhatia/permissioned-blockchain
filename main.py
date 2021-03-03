da8aec75ea1a7f2c12ee31cbd94ab3fc403c830d
SaajanBhatia <Saajan@ssbhatia.co.uk>
2021-03-03 22:31:15 +0000

    Blockchain Server Implementation

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from block import Block  
 
-blockchain = []
+
+class server():
+    def __init__(self, hostname, public_ip_addr):
+        self.__hostname = hostname
+        self._public_ip_addr = public_ip_addr
+        self.genesisBlock = Block("The Genesis Block of Alpoe",'Transactions example')
+    
+    def addBlock(self, transaction, prev_hash):
+        pass
+
+    def createTransaction(phase, status, value, quantity):
+        {
+            'Production Stage' : phase,
+            'Status' : status,
+            'Value/ Quantity' : quantity,
+            'Time' : '3 Mar 2021 11:45:34 GMT'
+        }
+
+
 
 ## Genesis Block - First Block in the Blockchain
 genesisBlock = Block("The Genesis Block of Alpoe",'Transactions example')
